e17d292df0efe9097a52c2b41af1f001284c63d6
Arnauld <arnauld.loyer@gmail.com>
2017-10-04 16:32:58 +0200

    copy repo back to initial commit

---
+++ MovieRental/main.swift
@@ -0,0 +1,22 @@
+//
+//  main.swift
+//  MovieRental
+//
+//  Created by xionggai on 1/1/16.
+//  Copyright © 2016 xionggai. All rights reserved.
+//
+
+import Foundation
+
+let 一个勺子 = Movie(title: "一个勺子", priceCode: .NewRelease)
+let 恶棍天使 = Movie(title: "恶棍天使", priceCode: .Children)
+
+let rental = Rental(movie: 一个勺子, daysRented: 7)
+let anotherRental = Rental(movie: 恶棍天使, daysRented: 1)
+
+let zhangsan = Customer(name: "zhangsan")
+zhangsan.addRental(rental)
+zhangsan.addRental(anotherRental)
+
+print(zhangsan.statement())
+
